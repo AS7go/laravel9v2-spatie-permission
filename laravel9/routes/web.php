976e0da5ee09ed4694d9dc38a10f50132c6dc699
Aleksandr <sh17aleksandr@gmail.com>
2024-05-13 14:35:15 +0300

    route admin super-user

--- laravel9/routes/web.php
+++ laravel9/routes/web.php
@@ -19,15 +19,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+    Route::get('add-post', [PostController::class, 'create'])->name('add-post');
+    Route::post('store-post', [PostController::class, 'store'])->name('store-post');
+    Route::get('edit-post/{id}', [PostController::class, 'edit'])->name('edit-post');
+    Route::put('update-post/{id}', [PostController::class, 'update'])->name('update-post');
+    Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('delete-post');
+});
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-Route::get('/dashboard',[PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
-Route::get('add-post',[PostController::class, 'create'])->name('add-post');
-Route::post('store-post',[PostController::class, 'store'])->name('store-post');
-Route::get('edit-post/{id}',[PostController::class, 'edit'])->name('edit-post');
-Route::put('update-post/{id}',[PostController::class, 'update'])->name('update-post');
-Route::delete('delete-post/{id}',[PostController::class, 'delete'])->name('delete-post');
+// Route::get('/dashboard',[PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
+// Route::get('add-post',[PostController::class, 'create'])->name('add-post');
+// Route::post('store-post',[PostController::class, 'store'])->name('store-post');
+// Route::get('edit-post/{id}',[PostController::class, 'edit'])->name('edit-post');
+// Route::put('update-post/{id}',[PostController::class, 'update'])->name('update-post');
+// Route::delete('delete-post/{id}',[PostController::class, 'delete'])->name('delete-post');
 
 require __DIR__ . '/auth.php';
